52ffae496bb98a98cf1dce6fac0de76d975186a2
mochi2000 <umaibou2000jp@yahoo.co.jp>
2021-01-14 22:53:18 +0900

    サイコロの表示

---
+++ main.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<!<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>サイコロ</title>
+</head>
+<body>
+  <h1>サイコロ</h1>
+  <p>サイコロの目は『』でした</p>
+  <p><a href="">もう一度</a></p>
+</body>
+</html>
